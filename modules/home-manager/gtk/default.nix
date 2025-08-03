1ed576c23b5caf924bd33ed9d9f6000d0df9b460
Aly Raffauf <aly@raffauflabs.com>
2025-08-03 09:24:35 -0400

    hm/{gnome,gtk} enable gtk and dconf

--- modules/home-manager/gtk/default.nix
+++ modules/home-manager/gtk/default.nix
@@ -6,9 +6,13 @@
   options.fontix.gtk.enable = lib.mkEnableOption "Gtk fonts";
 
   config = lib.mkIf config.fontix.gtk.enable {
-    gtk.font = {
-      inherit (config.fontix.fonts.sansSerif) name package;
-      size = config.fontix.sizes.applications;
+    gtk = {
+      enable = true;
+
+      font = {
+        inherit (config.fontix.fonts.sansSerif) name package;
+        size = config.fontix.sizes.applications;
+      };
     };
   };
 }
