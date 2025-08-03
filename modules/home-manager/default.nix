2710ddb01f0e06a312d7feda6163404d5a313e20
Aly Raffauf <aly@raffauflabs.com>
2025-08-02 23:16:03 -0400

    home-manager: add font options with defaults

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -1,1 +1,24 @@
-{}
+{
+  lib,
+  pkgs,
+  ...
+}: {
+  options.fontix = let
+    mkFontOption = name: default: package: {
+      name = lib.mkOption {
+        type = lib.types.str;
+        description = "Name of the ${name} font.";
+        inherit default;
+      };
+
+      package = lib.mkPackageOption pkgs package {
+        description = "Package providing the ${name} font.";
+      };
+    };
+  in {
+    emoji = mkFontOption "emoji" "Noto Color Emoji" "noto-fonts-color-emoji";
+    monospace = mkFontOption "monospace" "DejaVu Sans Mono" "dejavu_fonts";
+    sans = mkFontOption "sans" "DejaVu Sans" "dejavu_fonts";
+    serif = mkFontOption "serif" "DejaVu Serif" "dejavu_fonts";
+  };
+}
